7b41d07321894535e09360aeb63c4d18509cd3c9
mayambacode <bokambma@sheridancollege.ca>
2025-08-10 23:40:49 -0400

    fixed tab image issue

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,7 +4,7 @@ import { Tabs } from 'expo-router'
 import { images } from '@/constants/images'
 import { icons } from '@/constants/icons'
 
-const TabIcon = ({focused, icon, title}: any) => {
+const TabIcon = ({focused, icons, title}: any) => {
 
   if( focused) {
   return (
@@ -13,7 +13,7 @@ const TabIcon = ({focused, icon, title}: any) => {
                     className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 
                     justify-center items-center rounded-full overflow-hidden">
 
-                      <Image source={icon}
+                      <Image source={icons}
                             tintColor="#151312" className= "size-5"/>
 
                         <Text className="text-secondary text-base font-semibold ml-2">{title}</Text>
@@ -23,7 +23,7 @@ const TabIcon = ({focused, icon, title}: any) => {
   )}
   return (
     <View className='size-full justify-center items-center mt-4 rounded-full'>
-      <Image source={icon}
+      <Image source={icons}
              tintColor="#A8B5DB" className='size-5'/>
     </View>
   )
