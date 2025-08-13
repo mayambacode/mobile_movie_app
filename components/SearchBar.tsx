269a485ab8c999884305ac2efedad69087f7a58d
mayambacode <bokambma@sheridancollege.ca>
2025-08-13 02:51:52 -0400

    just changed the bg color of the search bar.
    but nothing happened.

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -10,7 +10,7 @@ interface Props {
 
 const SearchBar = ({ placeholder, onPress }: Props) => {
   return (
-    <View className="flex-row items-center bg-blue-500 rounded-full px-5 py-4">
+    <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
       <Image source={icons.search} className="size-5" resizeMode='contain' tintColor="ab8bff"/>
       <TextInput
         onPress={() => {}}
